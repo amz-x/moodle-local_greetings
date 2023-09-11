2d701c479acedb702b48b83a402c3b8f378cf893
Christopher Crouse <mail@amz-x.com>
2023-09-11 10:00:48 +0200

    add plugin link to navigation

--- lib.php
+++ lib.php
@@ -22,6 +22,12 @@
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+
+/**
+ * Greet logged in / not logged in user message in country locale.
+ *
+ * @param StdClass $user user object.
+ */
 function local_greetings_get_greeting($user) {
     if ($user == null || !isloggedin()) {
         return get_string('greetinguser', 'local_greetings', 'user');
@@ -38,4 +44,36 @@ function local_greetings_get_greeting($user) {
     }
 
     return get_string($langstr, 'local_greetings', fullname($user));
+}
+
+
+/**
+ * Insert a link to index.php on the site front page navigation menu.
+ *
+ * @param navigation_node $frontpage Node representing the front page in the navigation tree.
+ */
+function local_greetings_extend_navigation_frontpage(navigation_node $frontpage) {
+    $frontpage->add(
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+    );
+}
+
+/**
+ * Insert a link to index.php on the site sidebar navigation menu.
+ *
+ * @param global_navigation $root Node representing the root navigation tree.
+ */
+function local_greetings_extend_navigation(global_navigation $root) {
+    $node = navigation_node::create (
+        get_string('pluginname', 'local_greetings'),
+        new moodle_url('/local/greetings/index.php'),
+        navigation_node::TYPE_CUSTOM,
+        null,
+        null,
+        new pix_icon('t/message', '')
+    );
+
+    $root->add_node($node);
 }
